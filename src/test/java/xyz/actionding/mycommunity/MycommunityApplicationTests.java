bc4c7ecf8d77f9b59d50de72e40a6dbc290e7091
ActionDing <dingkj32@gmail.com>
2022-06-02 20:31:25 +0800

    Initial commit

---
+++ src/test/java/xyz/actionding/mycommunity/MycommunityApplicationTests.java
@@ -0,0 +1,14 @@
+package xyz.actionding.mycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
